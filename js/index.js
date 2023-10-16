fff66e883a242e5936a5c56bd70a3983389a7dec
Micole Trimmer <micole@onthecurb.io>
2023-10-15 22:01:36 -0400

    Added javascript file and a transition
    Transitioned from index.html to happening.html

---
+++ js/index.js
@@ -0,0 +1,5 @@
+        // Delay the transition to happening.html by 3 seconds
+        setTimeout(function() {
+            // Redirect to happening.html after the delay
+            window.location.href = 'happening.html';
+        }, 5000); // 3000 milliseconds = 3 seconds
