ce5c11ab7dd1af57ae1bb55253d0f6150d2fd11c
Micole Trimmer <micole@onthecurb.io>
2023-10-18 13:21:24 -0400

    updates to the site that include an accordion menu, pre-loader animation and a page transition.
    The accordion menu broke when I took a functional template I created and added it into the games.html. The functionality was dead in the water, but the formatting was good. The error in the inspect, shows an uncaught ReferenceError: $ which is undefined. I could not figure out how to resolve that successfully as it all appears as I would expect, but I also do not fully understand what I am looking at yet.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-        // Delay the transition to happening.html by 3 seconds
-        setTimeout(function() {
-            // Redirect to happening.html after the delay
-            window.location.href = 'happening.html';
-        }, 5000); // 3000 milliseconds = 3 seconds
+$(".toggle").click(function(){
+    $(this).find(".toggleContainer").toggleClass("active");
+    $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+  });
---
+++ js/transition.js
@@ -0,0 +1,13 @@
+// Preloader function
+
+function fade() {
+    //    $('.preloader').fadeOut("slow");
+    }
+    setTimeout(fade, 4500);
+    
+    
+    // Delay the transition to happening.html by 3 seconds
+        setTimeout(function() {
+    // Redirect to happening.html after the delay
+       window.location.href = 'happening.html';
+        }, 5000); // 3000 milliseconds = 3 seconds
