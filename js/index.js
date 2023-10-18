fcf99939c5e25710e9bf79e63b9c28caf38de57a
Micole Trimmer <micole@onthecurb.io>
2023-10-18 19:28:59 -0400

    reworked the happening.html and its related .css
    continued updating to styling and interactions.
    
    Frustrated with the alignment of everything. Page transitions won't work the way I need them, and i have a ton of debugging for that accordion. menu functionality.

--- js/index.js
+++ js/index.js
@@ -2,3 +2,6 @@ $(".toggle").click(function(){
     $(this).find(".toggleContainer").toggleClass("active");
     $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
   });
+
+
+  /* -------- Games: Accordian Menu NEED TO TROUBLESHOOT/DEBUG WHY THE FUNCTIONALITY IS BROKEN :( -------- */
--- js/transition.js
+++ js/transition.js
@@ -1,13 +1,14 @@
 // Preloader function
 
 function fade() {
-    //    $('.preloader').fadeOut("slow");
+    $('.preloader').fadeOut("slow");
     }
-    setTimeout(fade, 4500);
+setTimeout(fade, 4500);
     
-    
-    // Delay the transition to happening.html by 3 seconds
-        setTimeout(function() {
-    // Redirect to happening.html after the delay
-       window.location.href = 'happening.html';
-        }, 5000); // 3000 milliseconds = 3 seconds
+// Delay the transition to happening.html by 3 seconds
+setTimeout(function() {
+// Redirect to happening.html after the delay
+    window.location.href = 'happening.html';
+    }, 5000); // 3000 milliseconds = 3 seconds
+
+// NOT SURE WHY I CANNOT FADEIN or FADEOUT on all my page transitions when replicating the above code :(
